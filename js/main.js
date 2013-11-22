8e0ae6aa58a9bc468fd2c30e19f4b0b8e0f56eda
Jody Tate <jody@josephtate.com>
2013-11-22 15:20:18 -0800

    working with firebase

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-var quoteCtrl = function ($scope) {
-     $scope.quote = 'something';
-}
+// var quoteCtrl = function ($scope) {
+//      $scope.quote = 'something';
+// }
+
+angular.module('quotesModule', ['firebase']).controller('quoteCtrl', ['$scope', 'angularFire',
+    function($scope, angularFire) {
+      var ref = new Firebase('https://purpose-of-art.firebaseio.com/quotes');
+      angularFire(ref.limit(15), $scope, "quotes");
+      //$scope.quotation = 'Quote' + Math.floor(Math.random()*101);
+      $scope.addQuote = function() {
+        $scope.quotes[ref.push().name()] = {
+          author: $scope.author, quotation: $scope.quotation
+        };
+        $scope.quote = "";
+      }}]);
